ed433199b2195d1328c2c84acd1cfa94d1bab07d
Lilja Maria Stefansdottir <liljamaria@eduroam.nat.ru.is>
2018-09-18 18:49:35 +0000

    first tile, second column

--- Tile_traveler.py
+++ Tile_traveler.py
@@ -32,6 +32,12 @@ while position != 31:
         else:
             print("Not a valid direction!")
     
+    elif position == 21:
+        print("You can travel (N)orth")
+        direction = input("Direction: ")
+        if direction.lower() == "n":
+            position += 1
+        else:
+            print("Not a valid direction!")
     
-
         
